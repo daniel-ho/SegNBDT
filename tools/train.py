cd3038539ef8cb9eb4de845a267c0c787676d68c
Daniel Ho <danielho@berkeley.edu>
2020-03-19 22:25:52 -0700

    fixed induced graph name

--- tools/train.py
+++ tools/train.py
@@ -194,7 +194,7 @@ def main():
                                  weight=train_dataset.class_weights)
 
     from nbdt.loss import SoftTreeSupLoss
-    path_graph = 'data/cityscapes/graph-induced-hrnet-w18-v1.json'
+    path_graph = 'data/cityscapes/graph-induced-HRNet-w18-v1.json'
     path_wnids = 'data/cityscapes/wnids.txt'
     classes = [f'n{i}' for i in range(19)]
     criterion = SoftTreeSupLoss(path_graph, path_wnids, classes, criterion=criterion, tree_supervision_weight=10)
