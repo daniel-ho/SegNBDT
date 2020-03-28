8f2ef70e3a0e2f36af93d5ed2c113524e0acef50
Daniel Ho <danielho@berkeley.edu>
2020-03-27 21:57:12 -0700

    mixed up rectangle origin

--- tools/vis_erf.py
+++ tools/vis_erf.py
@@ -110,7 +110,7 @@ def main():
     min_i, max_i = np.min(nonzero_i), np.max(nonzero_i)
     nonzero_j = np.nonzero(np.sum(input_grad, axis=1))
     min_j, max_j = np.min(nonzero_j), np.max(nonzero_j)
-    rect_origin = (min_i, min_j)
+    rect_origin = (min_j, min_i)
     rect_h, rect_w = max_i-min_i, max_j-min_j
     rect = patches.Rectangle(rect_origin, rect_w, rect_h, 
         linewidth=1, edgecolor='g', facecolor='none')
@@ -121,8 +121,8 @@ def main():
     ax = fig.add_subplot(111)
     ax.imshow(input_grad, cmap='coolwarm')
     ax.add_patch(rect)
-    title_values = [erf_center_i, erf_center_j, rect_origin[0], rect_origin[1], rect_h, rect_w]
-    ax.set_title("Output Pixel: ({},{}), ERF Origin: ({},{}), ERF Size: {} x {}".format(*title_values))
+    title_values = [output.shape[2],output.shape[3],erf_center_i,erf_center_j,rect_origin[0],rect_origin[1],rect_h,rect_w]
+    ax.set_title("Output Size: {} x {}, Output Pixel: ({},{})\n ERF Origin: ({},{}), ERF Size: {} x {}".format(*title_values))
     save_path = os.path.join(final_output_dir, 'erf_{}_{}.png'.format(args.offset_j,args.offset_j))
     plt.savefig(save_path)
 
