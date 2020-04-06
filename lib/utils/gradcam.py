2622a3d36626306f13f4234291520a33bc6599c6
Daniel Ho <danielho@berkeley.edu>
2020-04-05 23:00:17 -0700

    initial gradcam code

---
+++ lib/utils/gradcam.py
@@ -0,0 +1,148 @@
+#!/usr/bin/env python
+# coding: utf-8
+#
+# Author:   Kazuto Nakashima
+# URL:      http://kazuto1011.github.io
+# Created:  2017-05-26
+
+from collections import Sequence
+
+import numpy as np
+import torch
+import torch.nn as nn
+from torch.nn import functional as F
+from tqdm import tqdm
+
+
+class _BaseWrapper(object):
+
+    def __init__(self, model):
+        super(_BaseWrapper, self).__init__()
+        self.device = next(model.parameters()).device
+        self.model = model
+        self.handlers = []  # a set of hook function handlers
+
+    def _encode_one_hot(self, labels):
+        one_hot = torch.zeros_like(self.logits).to(self.device)
+        one_hot.scatter_(1, labels, 1.0)
+        return one_hot
+
+    def forward(self, image):
+        self.image_shape = image.shape[2:]
+        self.logits = self.model(image)
+        self.probs = F.softmax(self.logits, dim=1)
+        return self.probs.sort(dim=1, descending=True)  # ordered results
+
+    def backward(self, labels):
+        """
+        Class-specific backpropagation
+        """
+        one_hot = self._encode_one_hot(labels)
+        self.model.zero_grad()
+        self.logits.backward(gradient=one_hot, retain_graph=True)
+
+    def generate(self):
+        raise NotImplementedError
+
+    def remove_hook(self):
+        """
+        Remove all the forward/backward hook functions
+        """
+        for handle in self.handlers:
+            handle.remove()
+
+
+class GradCAM(_BaseWrapper):
+    """
+    "Grad-CAM: Visual Explanations from Deep Networks via Gradient-based Localization"
+    https://arxiv.org/pdf/1610.02391.pdf
+    Look at Figure 2 on page 4
+    """
+
+    def __init__(self, model, candidate_layers=None):
+        super(GradCAM, self).__init__(model)
+        self.fmap_pool = {}
+        self.grad_pool = {}
+        self.candidate_layers = candidate_layers  # list
+
+        def save_fmaps(key):
+            def forward_hook(module, input, output):
+                self.fmap_pool[key] = output.detach()
+
+            return forward_hook
+
+        def save_grads(key):
+            def backward_hook(module, grad_in, grad_out):
+                self.grad_pool[key] = grad_out[0].detach()
+
+            return backward_hook
+
+        # If any candidates are not specified, the hook is registered to all the layers.
+        for name, module in self.model.named_modules():
+            if self.candidate_layers is None or name in self.candidate_layers:
+                self.handlers.append(module.register_forward_hook(save_fmaps(name)))
+                self.handlers.append(module.register_backward_hook(save_grads(name)))
+
+    def _find(self, pool, target_layer):
+        if target_layer in pool.keys():
+            return pool[target_layer]
+        else:
+            raise ValueError("Invalid layer name: {}".format(target_layer))
+
+    def generate(self, target_layer):
+        fmaps = self._find(self.fmap_pool, target_layer)
+        grads = self._find(self.grad_pool, target_layer)
+        weights = F.adaptive_avg_pool2d(grads, 1)
+
+        gcam = torch.mul(fmaps, weights).sum(dim=1, keepdim=True)
+        gcam = F.relu(gcam)
+        gcam = F.interpolate(
+            gcam, self.image_shape, mode="bilinear", align_corners=False
+        )
+
+        B, C, H, W = gcam.shape
+        gcam = gcam.view(B, -1)
+        gcam -= gcam.min(dim=1, keepdim=True)[0]
+        gcam /= gcam.max(dim=1, keepdim=True)[0]
+        gcam = gcam.view(B, C, H, W)
+
+        return gcam
+
+
+class _SegBaseWrapper(_BaseWrapper):
+
+    def _encode_one_hot(self, labels, pixel_i, pixel_j):
+        one_hot = torch.zeros_like(self.logits).to(self.device)
+        scattered = one_hot[:,:,pixel_i,pixel_j].scatter_(1, labels[:,:,pixel_i,pixel_j], 1.0)
+        one_hot[:,:,pixel_i,pixel_j] = scattered
+        return one_hot
+
+    def backward(self, labels, pixel_i, pixel_j):
+        """
+        Class and pixel specific backpropagation
+        """
+        one_hot = self._encode_one_hot(labels, pixel_i, pixel_j)
+        self.model.zero_grad()
+        self.logits.backward(gradient=one_hot, retain_graph=True)
+
+
+class SegGradCAM(_SegBaseWrapper, GradCAM):
+
+    def generate(self, target_layer):
+        fmaps = self._find(self.fmap_pool, target_layer)
+        grads = self._find(self.grad_pool, target_layer)
+        weights = grads
+
+        gcam = torch.mul(fmaps, weights).sum(dim=1, keepdim=True)
+        gcam = F.relu(gcam)
+        gcam = F.interpolate(
+            gcam, self.image_shape, mode="bilinear", align_corners=False
+        )
+
+        B, C, H, W = gcam.shape
+        gcam = gcam.view(B, -1)
+        gcam -= gcam.min(dim=1, keepdim=True)[0]
+        gcam /= gcam.max(dim=1, keepdim=True)[0]
+        gcam = gcam.view(B, C, H, W)
+
+        return gcam
---
+++ tools/vis_gradcam.py
@@ -0,0 +1,158 @@
+import argparse
+import os
+import pprint
+import shutil
+import sys
+
+import logging
+import time
+import timeit
+from pathlib import Path
+
+import cv2
+import numpy as np
+import matplotlib.cm as cm
+import matplotlib.pyplot as plt
+
+import torch
+import torch.nn as nn
+import torch.backends.cudnn as cudnn
+
+import _init_paths
+import models
+import datasets
+from config import config
+from config import update_config
+from core.function import testval
+from utils.gradcam import SegGradCAM
+from utils.modelsummary import get_model_summary
+from utils.utils import create_logger
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Visualize GradCAM')
+    
+    parser.add_argument('--cfg',
+                        help='experiment configure file name',
+                        required=True,
+                        type=str)
+    parser.add_argument('--image-index', type=int, default=0,
+                        help='Index of input image for GradCAM')
+    parser.add_argument('--pixel-i', type=int, default=0, 
+                        help='i coordinate of pixel from which to compute GradCAM')
+    parser.add_argument('--pixel-j', type=int, default=0, 
+                        help='j coordinate of pixel from which to compute GradCAM')
+    parser.add_argument('--target-layer', type=str,
+                        help='Target layer from which to compute GradCAM')
+    parser.add_argument('opts',
+                        help="Modify config options using the command-line",
+                        default=None,
+                        nargs=argparse.REMAINDER)
+
+    args = parser.parse_args()
+    update_config(config, args)
+
+    return args
+
+def retrieve_raw_image(dataset, index):
+    item = dataset.files[index]
+    image = cv2.imread(os.path.join(dataset.root,'cityscapes',item["img"]),
+                       cv2.IMREAD_COLOR)
+    return image
+
+def save_gradcam(save_path, gradcam, raw_image, paper_cmap=False):
+    gradcam = gradcam.cpu().numpy()
+    cmap = cm.jet_r(gradcam)[..., :3] * 255.0
+    if paper_cmap:
+        alpha = gradcam[..., None]
+        gradcam = alpha * cmap + (1 - alpha) * raw_image
+    else:
+        gradcam = (cmap.astype(np.float) + raw_image.astype(np.float)) / 2
+    cv2.imwrite(save_path, np.uint8(gradcam))
+
+def main():
+    args = parse_args()
+
+    logger, final_output_dir, _ = create_logger(
+        config, args.cfg, 'vis_gradcam')
+
+    logger.info(pprint.pformat(args))
+    logger.info(pprint.pformat(config))
+
+    # cudnn related setting
+    cudnn.benchmark = config.CUDNN.BENCHMARK
+    cudnn.deterministic = config.CUDNN.DETERMINISTIC
+    cudnn.enabled = config.CUDNN.ENABLED
+
+    # build model
+    model = eval('models.'+config.MODEL.NAME +
+                 '.get_seg_model')(config)
+
+    dump_input = torch.rand(
+        (1, 3, config.TRAIN.IMAGE_SIZE[1], config.TRAIN.IMAGE_SIZE[0])
+    )
+    logger.info(get_model_summary(model.cuda(), dump_input.cuda()))
+
+    if config.TEST.MODEL_FILE:
+        model_state_file = config.TEST.MODEL_FILE
+    else:
+        model_state_file = os.path.join(final_output_dir,
+                                        'best.pth')
+    logger.info('=> loading model from {}'.format(model_state_file))
+        
+    pretrained_dict = torch.load(model_state_file)
+    model_dict = model.state_dict()
+    pretrained_dict = {k[6:]: v for k, v in pretrained_dict.items()
+                        if k[6:] in model_dict.keys()}
+    for k, _ in pretrained_dict.items():
+        logger.info(
+            '=> loading {} from pretrained model'.format(k))
+    model_dict.update(pretrained_dict)
+    model.load_state_dict(model_dict)
+
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    model = model.to(device).eval()
+
+    # Retrieve input image corresponding to args.image_index
+    test_size = (config.TEST.IMAGE_SIZE[1], config.TEST.IMAGE_SIZE[0])
+    test_dataset = eval('datasets.'+config.DATASET.DATASET)(
+                        root=config.DATASET.ROOT,
+                        list_path=config.DATASET.TEST_SET,
+                        num_samples=None,
+                        num_classes=config.DATASET.NUM_CLASSES,
+                        multi_scale=False,
+                        flip=False,
+                        ignore_label=config.TRAIN.IGNORE_LABEL,
+                        base_size=config.TEST.BASE_SIZE,
+                        crop_size=test_size,
+                        downsample_rate=1)
+    image,_,_,_ = test_dataset[args.image_index]
+    image = image.unsqueeze(0).to(device)
+
+    # Define target layer
+    if args.target_layer:
+        target_layer = args.target_layer
+    else:
+        for name, module in self.model.named_modules()[::-1]:
+            if 'conv' in name:
+                target_layer = name
+            break
+    logger.info('Target layer set to {}'.format(target_layer))
+
+    # Run forward + backward passes
+    gradcam_args = [args.image_index, args.pixel_i, args.pixel_j]
+    logger.info('Running GradCAM on image {} at pixel ({},{})...'.format(*gradcam_args))
+    gradcam = SegGradCAM(model=model, candidate_layers=[target_layer])
+    pred_probs, pred_labels = gradcam.forward(image)
+    gradcam.backward(pred_labels[:,[0],:,:], args.pixel_i, args.pixel_j)
+
+    # Generate GradCAM + save heatmap
+    gradcam_region = gradcam.generate(target_layer=target_layer)[0,0]
+    save_path = os.path.join(final_output_dir, 
+        'gradcam-image-{}-pixel_i-{}-pixel_j-{}'.format(*gradcam_args))
+    raw_image = retrieve_raw_image(test_dataset, args.image_index)
+    logger.info('Saving GradCAM heatmap at {}...'.format(save_path))
+    save_gradcam(save_path, gradcam_region, raw_image)
+
+
+if __name__ == '__main__':
+    main()
