357e1b70f884f73331243ed5ea86b49068f04df4
Alvin Wan <alvinwan1@gmail.com>
2020-05-22 20:31:25 -0700

    added matching grid

--- tools/vis_grid.py
+++ tools/vis_grid.py
@@ -1,6 +1,15 @@
 import os
 from jinja2 import Template
 import glob
+import argparse
+from collections import defaultdict
+from pathlib import Path
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--row', choices=('sort', 'match'),
+                    default='sort')
+args = parser.parse_args()
+
 
 directories = sorted(os.listdir('.'))
 
@@ -10,13 +19,26 @@ for directory in directories:
         continue
     paths_per_dirs.append(sorted(glob.iglob(f'{directory}/*.jpg'), key=lambda s: int(s.split('-')[1])))
 
-paths_per_ims = list(zip(*paths_per_dirs))
+if args.row == 'sort':
+    paths_per_rows = list(zip(*paths_per_dirs))
+elif args.row == 'match':
+    name_to_paths = defaultdict(lambda: [])
+    for paths_per_dir in paths_per_dirs:
+        for path in paths_per_dir:
+            path = Path(path)
+            name_to_paths[path.name].append(str(path))
+
+    n = max([len(paths) for paths in name_to_paths.values()])
+    paths_per_rows = [paths for paths in name_to_paths.values() if len(paths) == n]
+else:
+    raise UserWarning(f'{args.include} is not a valid include mode.')
+
 
 template = Template('''
 <html>
     <body>
     <table border="0" style="table-layout: fixed;">
-    {% for paths_per_im in paths_per_ims %}
+    {% for paths_per_im in paths_per_rows %}
         <tr>
         {% for path in paths_per_im %}
             <td halign="center" style="word-wrap: break-word;" valign="top">
@@ -32,4 +54,4 @@ template = Template('''
 ''')
 
 with open('index.html', 'w') as f:
-    f.write(template.render(paths_per_ims=paths_per_ims, width=300))
+    f.write(template.render(paths_per_rows=paths_per_rows, width=300))
