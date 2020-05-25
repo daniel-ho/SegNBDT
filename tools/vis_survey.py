0562ac4b13a021e98bf3c411a8149cda7fe97c36
Alvin Wan <alvinwan1@gmail.com>
2020-05-24 22:11:09 -0700

    output vis survey

---
+++ tools/vis_survey.py
@@ -0,0 +1,93 @@
+import os
+from jinja2 import Template
+import sys
+
+
+files = sys.argv[1:]
+
+template = Template(
+"""
+<html>
+  <head>
+    <title>Figure Browser</title>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/d3/3.5.17/d3.min.js"></script>
+    <script
+      src="https://code.jquery.com/jquery-3.5.1.min.js"
+      integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0="
+      crossorigin="anonymous"></script>
+    <script>
+var index = -1;
+var files = [
+    {% for file in files %}
+        '{{ file }}',
+    {% endfor %}
+];
+
+function select(i) {
+  i = parseInt(i);
+  index = i;
+  $('#curr').html(i);
+
+  $('svg').remove();
+  $('main').load(files[i], function() {
+    console.log('Loaded ' + i);
+  })
+
+  if (i <= 0) {
+    $('#prev').hide();
+  } else {
+    var url = '?id=' + (i-1);
+    $('#prev').show().attr('i', i-1).attr('href', '?id=' + (i-1));
+    $('#prev span').html(i - 1);
+    history.pushState('data', '', url);
+  }
+
+  if (i >= files.length - 1) {
+    $('#next').hide();
+  } else {
+    var url = '?id=' + (i+1);
+    $('#next').show().attr('i', i+1).attr('href', url);
+    $('#next span').html(i + 1);
+    history.pushState('data', '', url);
+  }
+}
+
+function onLoadSelect() {
+  const urlParams = new URLSearchParams(window.location.search);
+  const i = urlParams.get('id') || 0;
+  select(i);
+}
+
+$(document).ready(function() {
+  onLoadSelect();
+
+  $('#next').on('click', function(e) {
+      select($('#next').attr('i'));
+      e.preventDefault();
+  });
+
+  $('#prev').on('click', function(e) {
+      select($('#prev').attr('i'));
+      e.preventDefault();
+  });
+
+  $(window).on('popstate', onLoadSelect);
+});
+    </script>
+  </head>
+  <body>
+    <header>
+      <p>
+        <span>Figure ID: <b id="curr">-1</b></span>
+        <a href="#" id="prev">prev (<span>-1</span>)</a>
+        <a href="#" id="next">next (<span>-1</span>)</a>
+      </p>
+    </header>
+    <main>
+    </main>
+  </body>
+</html>
+""")
+
+with open("index.html", "w") as f:
+    f.write(template.render(files=files))
