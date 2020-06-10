842adb04ac73966a6be0bd25af6d3224aac66d77
Daniel Ho <danielho@berkeley.edu>
2020-06-09 22:00:10 -0700

    Revert "renamed ade20k to ade20k scene parsing"
    This reverts commit ea34349fae41fb7f794128a80cb772e3c7ad28cd.

--- lib/datasets/ade20k.py
+++ lib/datasets/ade20k.py
@@ -9,7 +9,7 @@ import torch
 
 from .base_dataset import BaseDataset
 
-class ADE20KSceneParsing(BaseDataset):
+class ADE20K(BaseDataset):
     def __init__(self, 
                  root,
                  list_path,
@@ -26,7 +26,7 @@ class ADE20KSceneParsing(BaseDataset):
                  mean=[0.485, 0.456, 0.406],
                  std=[0.229, 0.224, 0.225]):
 
-        super(ADE20KSceneParsing, self).__init__(ignore_label, base_size,
+        super(ADE20K, self).__init__(ignore_label, base_size,
                 crop_size, downsample_rate, scale_factor, mean, std)
 
         self.root = root
@@ -47,8 +47,8 @@ class ADE20KSceneParsing(BaseDataset):
     def read_files(self):
         files = []
         for item in self.img_list:
-            image_path = item['fpath_img'].replace('ADEChallengeData2016', 'ade20k_scene_parsing')
-            label_path = item['fpath_segm'].replace('ADEChallengeData2016', 'ade20k_scene_parsing')
+            image_path = item['fpath_img'].replace('ADEChallengeData2016', 'ade20k')
+            label_path = item['fpath_segm'].replace('ADEChallengeData2016', 'ade20k')
             name = os.path.splitext(os.path.basename(image_path))[0]
             files.append({
                 'img': image_path,
